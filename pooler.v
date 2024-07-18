bb8f95e100ba17a9bf67c5c973ac71fab87dafdd
Ohm-Rishabh <119029179+Ohm-Rishabh@users.noreply.github.com>
2024-07-19 01:29:52 +0530

    Update pooler.v

--- pooler.v
+++ pooler.v
@@ -36,34 +36,51 @@ module pooler #(
 	    global_rst,
 	    end_op
       );
-    
-    comparator2 #(.N(N),.ptype(ptype)) cmp(
-        ce,         
-	    data_in,
-	    mux_out,
-	    comp_op
-      );
-  
-    max_reg #(.N(N)) m1(               
-    	clk,
-    	ce,
-	    comp_op,
-	    rst_m,
-	    master_rst,
-	    max_reg_op
-      );
- 
-    variable_shift_reg #(.WIDTH(N),.SIZE((m/p))) SR (
-         .d(comp_op),                 
-         .clk(clk),                 
-         .ce(load_sr),                 
-         .rst(global_rst && master_rst),         
-         .out(sr_op)             
-         );
 
-   input_mux #(.N(N)) mux(sr_op,max_reg_op,sel,mux_out);
-   
-   qmult #(N,Q) mul (clk,rst_m,max_reg_op,p_sqr_inv,div_op,ovr); 
+
+
+	always @(ce || data_in || mux_out) begin
+		if (data_in > mux_out) begin
+			comp_op <= data_in;
+		end else begin
+			comp_op <= mux_out;
+		end
+	end
+
+	always @(posedge clk) begin
+		if (ce)  begin
+			reg_op <= comp_op;
+		end
+	end
+
+	reg [WIDTH-1:0] sr [SIZE-1:0];
+	generate
+	genvar i;
+	for(i=0;i<m/p;i=i+1)
+	begin
+	    always@(posedge clk or posedge master_rst)
+	    begin
+	    if(rst)
+	    begin
+	        sr[i] <= 'd0;
+	    end
+	    else if(ce)
+	        begin
+	            if(i == 'd0)
+	            begin
+	                sr[i] <= d;
+	            end
+	            else
+	            begin
+	                sr[i] <= sr[i-1];
+	            end
+	        end
+	    end
+	end
+
+	assign sr_op = sr[SIZE-1];
+
+	assign mux_out = (sel == 2'b01) ? sr_op : ((sel == 2'b00) ? reg_op: 0);
     
-   assign data_out = ptype ? max_reg_op : div_op; //for average pooling, we output the sum divided by p**2 
+   assign data_out = reg_op; 
 endmodule
